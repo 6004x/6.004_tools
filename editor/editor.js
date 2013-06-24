aed262d9de20018dda63ec1fddafe9878ad39c2b
Matthew Powers-Freeling <mattpf@mit.edu>
2013-06-24 15:57:29 -0400

    Basic editor functionality works.

---
+++ editor/editor.js
@@ -0,0 +1,129 @@
+var Editor = function(container, mode) {
+    var self = this; // Tracking 'this' can be finicky; use 'self' instead.
+    var mContainer = $(container);
+    var mToolbarHolder;
+    var mTabHolder;
+    var mCurrentDocument = null;
+    var mSyntaxMode = mode;
+
+    var mOpenDocuments = {}; // Mapping of document paths to editor instances.
+
+    this.addButtonGroup = function(buttons) {
+        var group = $('<div class="btn-group">');
+        _.each(buttons, function(button) {
+            button.render(group);
+        });
+        mToolbarHolder.append(group);
+    };
+
+    this.focusTab = function(doc) {
+        doc.tab.find('a').tab('show');
+        if(mCurrentDocument) mCurrentDocument.el.hide();
+        doc.el.show().addClass('active');
+        doc.cm.refresh();
+        mCurrentDocument = doc;
+    };
+
+    this.unfocusTab = function(doc) {
+        if(!doc) return;
+        doc.el.removeClass("active").hide();
+        doc.tab.removeClass('active');
+    }
+
+    var create_cm_instance = function(container, content) {
+        var cm = new CodeMirror(container[0], {
+            indentUint: 4,
+            lineNumbers: true,
+            electricChars: true,
+            matchBracktes: true,
+            autoCloseBrackets: true,
+            smartIndent: true,
+            indentWithTabs: true,
+            styleActiveLine: true,
+            value: content,
+            mode: mode
+        });
+        cm.on('save', function() {
+            alert("This would save, if it had anything to save to.");
+        });
+        return cm;
+    }
+
+    this.openTab = function(filename, content, activate) {
+        // We need this for the tabs to do anything useful.
+        var id = _.uniqueId('edit_tab');
+        var editPane = $('<div>', {'class': 'tab-pane', id: id}).hide();
+        var cm = create_cm_instance(editPane, content);
+        var tab = $('<li>');
+
+        var doc = {el: editPane, tab: tab, cm: cm};
+
+        var a = $('<a>', {href: '#' + id}).text(filename).click(function(e) { e.preventDefault(); self.focusTab(doc); });
+        tab.append(a);
+
+        mTabHolder.append(tab);
+
+        mContainer.append(editPane);
+
+        if(!_.size(mOpenDocuments) || activate) {
+            self.unfocusTab(mCurrentDocument);
+            self.focusTab(doc);
+        }
+
+        // Stash these away somewhere.
+        mOpenDocuments[filename] = doc;
+    };
+
+    this.tab = function(filename) {
+        return mOpenDocuments[filename];
+    };
+
+    var initialise = function() {
+        // Build up our editor UI.
+        mToolbarHolder = $('<div class="btn-toolbar">');
+        // Add some basic button groups
+        self.addButtonGroup([new ToolbarButton('icon-file'), new ToolbarButton('icon-refresh'), new ToolbarButton('icon-hdd')]);
+        mContainer.append(mToolbarHolder);
+
+        // Add something to hold our editor tabs
+        mTabHolder = $('<ul class="nav nav-tabs">');
+        mContainer.append(mTabHolder);
+
+        // Do some one-time setup.
+        if(!Editor.IsSetUp) {
+            CodeMirror.commands.save = do_save;
+        }
+    };
+
+    var do_save = function() {
+        alert("This would save if there was anywhere useful to save to.");
+    };
+
+    initialise();
+};
+Editor.IsSetUp = false;
+
+var ToolbarButton = function(icon, callback, tooltip) {
+    var self = this;
+    var mTooltip = tooltip;
+    var mIcon = icon;
+    var mCallback = callback;
+    var mElement = null;
+
+    self.render = function(container) {
+        if(mElement) {
+            mElement.remove();
+            mElement = null;
+        }
+        // Special case: if 'icon' is a string starting with 'icon-', assume they wanted a Bootstrap icon.
+        // Otherwise it's whatever jQuery makes of it.
+        if(/^icon-/.test(mIcon)) {
+            mIcon = $('<i>').addClass(mIcon);
+        }
+        mElement = $('<button class="btn">').append(mIcon);
+        if(tooltip) {
+            mElement.tooltip({title: tooltip, placement: 'bottom', delay: 100});
+        }
+        container.append(mElement);
+    };
+};
