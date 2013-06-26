dae06e9b3c9fd0d21a63a43bf9ff1f6f7fce8650
Stacey Terman <seterman@mit.edu>
2013-06-26 14:20:37 -0400

    working on string-level replacement

--- parser.js
+++ parser.js
@@ -1,27 +1,64 @@
 var parser = (function(){
     
+/**********************************
+Analyzer (string level): removes comments and line extensions
+************************************/
+    function analyze(input_string){
+        input_string += "\n";
+        var line_ext_pattern = /(\n+)(\s*\+)/g;
+        // newline followed by (whitespace and) a plus
+        var comment1_pattern = /\/\/.*\n/g;
+        var comment2_pattern = /\/\*.*\*\//g;
+        // double slash till the end of the line
+        // slash-star till star-slash
+        
+        // extend lines by replacing the line_ext_pattern with a
+        // record separator for each newline.
+        var extended_string = input_string.replace(line_ext_pattern, 
+            function(match,newlines,space_plus){
+                // newlines is the string of newlines before the plus
+                // space_plus is the whitespace and plus sign
+                return newlines.replace(/\n/g,"%"/*"\u001e"*/);      
+        });
+//        console.log("extended string:",extended_string);
+        
+        // remove single-line comments by replacing them with a blank string
+        var decommented_string = extended_string.replace(comment1_pattern,"");
+        
+        // remove multi-line comments by replacing them with a record
+        // separator for each newline
+        decommented_string = decommented_string.replace(comment2_pattern,
+            function(match,newlines){
+                // newlines is all newlines in the comment (hopefully)
+                return newlines.replace(/\n/g,"%"/*"\u001e"*/);
+            });
+        
+        return decommented_string;
+    }
+    
+/*****************************
+Splitter: splits a string into an array of tokens
+*******************************/
     function split(input_string){
-        var pattern = /\/\/|\/\*|\*\/|\*|\+|".*"|0x[0-9a-fA-F]+|0[0-7]+|-?\d*\.?\d+(([eE]-?\d+)|[a-zA-Z]+)?|\.?[A-Za-z][\w:\.,$\[\]]*|=|\n/g; 
+        var pattern = /".*"|0x[0-9a-fA-F]+|-?\d*\.?\d+(([eE]-?\d+)|[a-zA-Z]*)|\.?[A-Za-z][\w:\.,$#\[\]]*|=|\n|\u001e/g; 
         
         var names_pattern = /[A-Za-z][\w,$:\[\]\.]*/;
+        var int_pattern = /\d+/;
+        var exp_pattern = /-?\d*\.?\d+[eE]-?\d+/;
+        var num_pattern = /-?\d*\.?\d+([A-Za-z]*|[eE]-?\d+)/;
         
-        // matches, in order:
-        //      an astarisk
-        //      two forward slashes
-        //      a forward slash followed by a star
-        //      a star  followed by a forward slash
-        //      a (plain) star
-        //      a plus sign
+        // 'pattern' matches, in order:
         //      anything wrapped in quotes
-        //      an optional minus sign, zero or more digits, an optional decimal point, one or more digits, and either an optional exponent term consisting of an e or E, optional negative sign, and one or more digits; or a scale factor
-        //      an optional period, followed by a letter, then any number of letters, digits, underscores, colons, dollar signs, square brackets, commas, or periods
+        //      a hex number
+        //      a number (int, float, exponential or scaled) (includes octal)
+        //      names:
+        //          a sequence that may start with a period,
+        //          then has a letter, 
+        //          then any number of letters, numbers, underscores, colons,
+        //          periods, commas, dollar signs, number signs, or square brackets
+        //      an equals sign
         //      a newline
-        
-        // comments = \/\/|\/\*|\*\/|\*
-        // nums_noscale = -?\d*\.?\d+([eE]-?\d+?
-        // nums_scale = -?\d*\.?\d+[TtGg(MEG)(meg)KkMmUu(MIL)(mil)NnPpF]
-        // names = \.?[A-Za-z][\w:\.,$\[\]]*
-        // newlines = \n/g
+        //      a record separater
         
         var matched_array;
         var substrings = [];
@@ -30,18 +67,24 @@ var parser = (function(){
             var type;
             if (names_pattern.test(matched_array[0])){
                 type = 'name';
+            } else if (int_pattern.test(matched_array[0])){
+                type = 'int';
+            } else if (exp_pattern.test(matched_array[0])){
+                type = 'exp';
             } else {
                 type = null;   
             }
-            substrings.push({token:matched_array[0],
-                             line:lineNumber,
-                             type:type
-                            });
-            if (matched_array[0] == "\n"){
+            if (!(matched_array[0]=="\u001e")){
+                substrings.push({token:matched_array[0],
+                                 line:lineNumber,
+                                 type:type
+                                });
+            }
+            if ((matched_array[0] == "\n")||(matched_array[0] == "\u001e")){
                 lineNumber += 1;
             }
         }
-        console.log(substrings);
+//        console.log(substrings);
         return substrings;
     }
     
@@ -59,72 +102,97 @@ otherwise, return false
 /*********************************
 Line extender: look for + signs directly following newlines and remove both
 *********************************/
-    function line_extend(token_array){
-//        console.log("initial array:",token_array);
-        
-        var extended_array = [];
-        
-        while(token_array.length > 0){
-            if (match_token("\n",token_array) && match_token("+",token_array.slice(1))){
-                token_array.shift();
-                token_array.shift();
-            } else {
-                extended_array.push(token_array.shift());
-            }
-        }
-        
-//        console.log("extended array:",extended_array);
-        return extended_array;
-    }
+//    function line_extend(token_array){
+////        console.log("initial array:",token_array);
+//        
+//        var extended_array = [];
+//        
+//        while(token_array.length > 0){
+//            if (match_token("\n",token_array) && match_token("+",token_array.slice(1))){
+//                token_array.shift();
+//                token_array.shift();
+//            } else {
+//                extended_array.push(token_array.shift());
+//            }
+//        }
+//        
+////        console.log("extended array:",extended_array);
+//        return extended_array;
+//    }
     
 /*****************************
 Uncommenter: given an array of raw substrings, remove the comments
 *****************************/
-    function decomment(token_array){
-//        console.log("decomment's token array:",token_array);
-        var nocomment_array = [];
-        
-        while(token_array.length > 0){
-            // comment type 1: double slash till end of line
-            if (match_token("//",token_array)){
-                while(!match_token("\n",token_array)){ 
-                      token_array.shift(); 
-                }
-            // comment type 2: C-style multiline comment /* ... */
-            } else if (match_token("/*",token_array)) {
-                while(!match_token("*/",token_array)&&!(token_array.length==1)){
-                    token_array.shift(); 
-                }
-                if (token_array[0].token != "*/"){
-                    throw "Unclosed comment at line "+token_array[0].line+".";
-                }
-                token_array.shift();
-            } else {
-                // if the current token is not part of a comment, simply transfer it
-                // to the new array
-                nocomment_array.push(token_array.shift());
-            } 
-        }
-//        console.log("uncommented array:",nocomment_array);
-        return nocomment_array;
-    }
+//    function decomment(token_array){
+////        console.log("decomment's token array:",token_array);
+//        var nocomment_array = [];
+//        
+//        while(token_array.length > 0){
+//            // comment type 1: double slash till end of line
+//            if (match_token("//",token_array)){
+//                while(!match_token("\n",token_array)){ 
+//                      token_array.shift(); 
+//                }
+//            // comment type 2: C-style multiline comment /* ... */
+//            } else if (match_token("/*",token_array)) {
+//                while(!match_token("*/",token_array)&&!(token_array.length==1)){
+//                    token_array.shift(); 
+//                }
+//                if (token_array[0].token != "*/"){
+//                    throw "Unclosed comment at line "+token_array[0].line+".";
+//                }
+//                token_array.shift();
+//            // comment type 3: * at the beginning of a line
+////            } else if (match_token("\n",token_array) && 
+////                       match_token("*",token_array.slice(1))){
+////                token_array.shift();
+////                while(!match_token("\n",token_array)){ 
+////                      token_array.shift(); 
+////                }    
+//            } else {
+//                // if the current token is not part of a comment, simply transfer it
+//                // to the new array
+//                nocomment_array.push(token_array.shift());
+//            } 
+//        }
+////        console.log("uncommented array:",nocomment_array);
+//        return nocomment_array;
+//    }
     
 /********************************
 Iterater expander: expands iterators such as A[0:5] into the proper sequence
+                    and duplicators such as B#3
 *********************************/
     var iterator_pattern = /\[\d+:\d+(:-?\d+)?\]/;
-    // iterator syntax: [digit:digit(:optional_+/-digit)]   
+    var duplicator_pattern = /#\d+$/;
+    // iterator syntax: [digit:digit(:optional_+/-digit)] 
+    // duplicator syntax: anything#digit
 
     function iter_expand(token_array){
         var expanded_array = [];
         
         while (token_array.length > 0){
             current = token_array[0];
-            console.log("current token obj:",current);
+//            console.log("current token obj:",current);
             var iter_match_array;
+            var dupe_match_array;
+            var duped_token_array = [];
             var new_token_array = [];
             
             if (current.type=='name'){
+                if((dupe_match_array=duplicator_pattern.exec(current.token))!==null){
+                    var repetitions = parseInt(dupe_match_array[0].slice(1));
+                    var dupe_string = current.token.slice(0,dupe_match_array.index);
+                    for (var i=0; i<repetitions; i+=1){
+                        duped_token_array.push({token:dupe_string,
+                                             line:current.line,
+                                             type:'name'
+                                            });
+                    }
+                    duped_token_array = iter_expand(duped_token_array);
+                    expanded_array = expanded_array.concat(duped_token_array);
+                    token_array.shift();
+                }else
                 if((iter_match_array=iterator_pattern.exec(current.token))!==null){
                     var iter_string = iter_match_array[0];
                     console.log("iter string:",iter_string);
@@ -151,7 +219,7 @@ Iterater expander: expands iterators such as A[0:5] into the proper sequence
                 expanded_array.push(token_array.shift());
             }
         }
-        console.log("expanded array:",expanded_array);
+//        console.log("expanded array:",expanded_array);
         return expanded_array;
     }
     
@@ -203,10 +271,11 @@ strings of the form '[a]', '[a+k]', '[a+2k]', ..., '[b]'
     }
     
     return {split:split,
-            decomment:decomment,
-            line_extend:line_extend,
+//            decomment:decomment,
+//            line_extend:line_extend,
             iter_interpret:iter_interpret,
-            iter_expand:iter_expand
+            iter_expand:iter_expand,
+            analyze:analyze
               }
 }());
 
@@ -214,9 +283,9 @@ strings of the form '[a]', '[a+k]', '[a+2k]', ..., '[b]'
 testing
 ***************************/
 var raw_text = ''
-+ '*hi\n'
++ '//hi\n'
 + '+ this is still a comment\n'
-+ '.include "foo.txt"'
++ '.include "foo.txt"\n'
 + 'R1 a b 3k foo=7 //resistor\n'
 + 'Cthis:is_a,long$name[] a b 1\n'
 + 'Ra[0:2] a b 100\n'
@@ -224,11 +293,13 @@ var raw_text = ''
 + '.plot foo.bar.baz.bim\n'
 + '//&%#blarg@!~`'
 + '0xDEADBEEF  0x12345678 10u 8ks';
-var test2_text = ".plot foo //comment\n+still_a_comment\nR1 0 /* random comment2 */ 1 10\n/* test */";
+var test2_text = "comment1 \n+ comment2\n foobar 17 *comment3";
 var test3_text = "R1 0 1 10\nR2 1 2 \n+ 50";
 var test4_text = "name name2 a1 aa1 123 + \n foo.bar.biz.bam A0[3:0]\n"
 var iter_test_array = ["[4:0]","[4:0:2]","[0:4]","[0:4:2]",
                        "[0:4:0]","[1:4:2]","[0:4:3]"];
+var line_ext_text = "hi \n foo\n\n+ bar";
+var decomment_text = "not_a_comment1\n//comment1\n+still_comment1\nnot_a_comment2/*comment2*/\n/*comment3\n\nstill_comment3*/";
 
 function test1(){parser.split(raw_text);}
 function test2(){parser.decomment(parser.line_extend(parser.split(test2_text)));}
@@ -243,3 +314,7 @@ function test5(){
     }
 }
 function test6(){ parser.iter_expand(parser.split("Rtest a[0:1][2:3][4:5] ")); }
+function test7(){ return parser.iter_expand(parser.split("A[0:2] B#3 C[0:1]#2")); }
+function test8(){ return parser.decomment(parser.split("*hi \n foo \n bar *comment\n")); }
+function test9(){ console.log(parser.split(parser.analyze(raw_text))); }
+function test10(){ return parser.analyze(decomment_text); }
