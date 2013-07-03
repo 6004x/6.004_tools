a49451c45d798766a732ed490e026796458be535
Antonio Moreno <dontony@mit.edu>
2013-07-03 10:46:09 -0400

    added localStorage support, messed around with animation, separated fileSystem

--- ui_test/gui_test.js
+++ ui_test/gui_test.js
@@ -3,9 +3,13 @@ var GUI=new function(){
     var rootNode, editor, sideBarWrapper, editorWrapper;
     var openFiles=[];
     var fileSystem;
+
+
+    //attaches file list to the default node
     function refreshFileList(){
         getFileList(rootNode.find('.filePaths'));
     }
+
     function getFileList(parentNode){
 
         username= $('#user_input').val();
@@ -151,6 +155,7 @@ var GUI=new function(){
         rootNode.prepend(alert);
 
     }
+
 //current tab is file name
 //.content get content of current tab
     function saveCurrentFile(){
@@ -172,11 +177,43 @@ var GUI=new function(){
 
     }
 
-    function setup(root, fileSys){
+   
+    function addModals(){
+
+    }
+    function newFolder(){
+        var folderName=window.prompt('What is the name of the new folder you wish to make');
+        fileSystem.newFolder(username, folderName, function(file, status){
+            console.log(status);
+                refreshFileList();
+        });
+    }
+    function newFile(file_path){
+        var fileName=window.prompt('What is the name of the new file you wish to make');
+        var new_file=new Object();
+        new_file.name=file_path+fileName;
+        new_file.data='';
+        fileSystem.newFile(username, new_file, function(data, status){
+            console.log(data.status);
+            if(data.status=='success'){
+                displayFile(new_file);
+                refreshFileList();
+            }
+            else
+            {
+                alert('not successful');
+            }
+        });
+
+    }
+    function commit(){
+        console.log('commited?');
+    }
+     function setup(root, fileSys, editorN){
         rootNode=$(root);
         var wrapper=addDiv().addClass('row-fluid wrapper');
         sideBarWrapper=addDiv().addClass('span2 sideBarWrapper');
-        editorWrapper=addDiv().addClass('span10 folderStruct');
+        //editorWrapper=addDiv().addClass('span10 folderStruct');
 
         var buttonDiv=addDiv().addClass('btn-group group1 buttonDiv');
         addButtons(buttonDiv);
@@ -185,68 +222,31 @@ var GUI=new function(){
         var filePaths=$('<ul></ul>').addClass('nav nav-list nav-stacked filePaths');
         sideBarNav.append(filePaths);
 
-        sideBarWrapper.append(buttonDiv);
-        sideBarWrapper.append(sideBarNav);
 
         
-        var rowOne=addDiv().addClass('row').append(sideBarWrapper).append(editorWrapper);
-        wrapper.append(rowOne);
+        //var rowOne=addDiv().addClass('row').append(sideBarWrapper).append(editorWrapper);
+        //wrapper.append(rowOne);
         var tempName=$("<h1 class='testDiv'>testing</h1>USERNAME:"
-            +"<input id = 'user_input' type='text' title='username'></input>"
-            +"SERVER:<input id = 'server_input' type='text' title='server'></input>"
-            +"<button class='btn btn-info' id='user_button'>get filelist</button>"
-            +"<div class = 'modeButtons btn-group'>"
-            +"<button class='btn btn-success' id='jsim'><img src=js.png></img>IM</button>"
-            +"<button class='btn btn-success' id='bsim'><em>&beta;</em>sim</button>"
-            +"<button class='btn btn-success' id='tmsim'>TMsim</button></div>");
-      
+            +"<button class='btn btn-info' id='user_button'>get filelist</button>");
 
-        rootNode.append(tempName);
-
+        sideBarWrapper,append(tempName);
+        sideBarWrapper.append(buttonDiv);
+        sideBarWrapper.append(sideBarNav);
+        editor=editorN;
         fileSystem=fileSys;
         setSyntax();
-        rootNode.append(wrapper);
+        rootNode.append(sideBarWrapper);
         
         $('#user_button').on('click', function(e){
                 console.log('button');
                 rootNode.find('.filePaths').html('');
                 refreshFileList();
             });
-        $('#jsim').on('click', function (e){setSyntax('jsim');});
-        $('#bsim').on('click', function (e){setSyntax('bsim');});
-        $('#tmsim').on('click', function (e){setSyntax('tmsim');});
 
         $('.btn').tooltip({'placement': 'bottom'});
-        rootNode=wrapper;
     }
     function addDiv(){
         return $('<div></div>');
-    }
-    function setSyntax(mode){
-        mode=mode||'jsim';
-        console.log(mode);
-        editorWrapper.html('');
-        editor = new Editor(editorWrapper, 'jsim');
-        editor.addButtonGroup([new ToolbarButton('Run', _.identity, 'Runs your program!'), 
-            new ToolbarButton('Export'), 
-            new ToolbarButton('Save', saveCurrentFile, 'Saves the current File')]);
-
-        editor.addButtonGroup([new ToolbarButton('show folders',showNavBar, '')]);
-
-
-
-        var set_height = function() {
-                editor.setHeight(document.documentElement.clientHeight - 70); // Set height to window height minus title.
-        }
-        set_height();
-        $(window).resize(set_height);
-        if(openFiles.length>0)
-            $.each(openFiles, function(i, file){
-                console.log(i)
-                console.log(file)
-                editor.openTab(file.name, file.data, true);
-            });
-
     }
     function addButtons(buttonDiv){   
             var buttonZero=$('<button></button>').addClass('btn hideNavBar').attr({
@@ -284,102 +284,25 @@ var GUI=new function(){
                     });
             buttonFour.append('<i class=icon-off></i>');
             buttonDiv.append(buttonFour);
+
+            //writing listeners
             buttonTwo.on('click', function(e){
                 $('.filePaths').html('');
                 refreshFileList();
             });
             buttonZero.on('click', function(e){
-                hideNavBar();
-                console.log('hide');
+                //hideNavBar();
+                console.log('hide feature unimplemented');
             });
             buttonOne.on('click', function(e){
                 newFolder();
                 console.log('new');
             })
     }
-    function addModals(){
-
-    }
-    function newFolder(){
-        var folderName=window.prompt('What is the name of the new folder you wish to make');
-        fileSystem.newFolder(username, folderName, function(file, status){
-            console.log(status);
-                refreshFileList();
-        });
-    }
-    function newFile(file_path){
-        var fileName=window.prompt('What is the name of the new file you wish to make');
-        var new_file=new Object();
-        new_file.name=file_path+fileName;
-        new_file.data='';
-        fileSystem.newFile(username, new_file, function(data, status){
-            console.log(data.status);
-            if(data.status=='success'){
-                displayFile(new_file);
-                refreshFileList();
-            }
-            else
-            {
-                alert('not successful');
-            }
-        });
-
-    }
-    function hideNavBar(){
-        sideBarWrapper.css('position', 'relative');
-        var width=-(sideBarWrapper.width());
-
-        console.log(width);
-        sideBarWrapper.animate({'left' :width}, 500, 'swing', function(){
-                sideBarWrapper.detach()
-                addFifthButton();
-
-        });
-        var offset=-editorWrapper.offset().left+parseInt(editorWrapper.css('margin-left'));
-        editorWrapper.animate({'left' :offset}, 500, 'swing', function(){
-            editorWrapper.removeClass('span10');
-            editorWrapper.css('left', 0);
-        });
-
-        function addFifthButton(){
-            
-
-        }
-    }
-    function showNavBar(){
-        if(sideBarWrapper.parent().length==0){
-            console.log(sideBarWrapper.parent());
-            rootNode.find('.row').prepend(sideBarWrapper);    
-            var width=(sideBarWrapper.width());
-            editorWrapper.addClass('span10');
-            editorWrapper.css('left', -width);
-            console.log(width);
-            editorWrapper.removeClass('float-right');
-            
-            sideBarWrapper.animate({'left' :0}, 500, 'swing', function(){
-                
-            });
-            editorWrapper.animate({'left' :0}, 500, 'swing', function(){
-                
-            });
-        }
-    }
-    function commit(){
-        console.log('commited?');
-    }
 
     return {setup:setup};
 }();
 
 
-$(document).ready(function (){
-    
 
-    fileSystem.setup('http://localhost:8080');
-    GUI.setup('.wrapperDiv', fileSystem);
-    $('#user_input').val('dontony');
-    $('#server_input').val('http://localhost:8080');
-    
-});
-        
 
