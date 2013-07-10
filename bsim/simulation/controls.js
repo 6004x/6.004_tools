d4d0bc5dd4bcb5706e0c1e5ad82f07f049433a0a
Matthew Powers-Freeling <mattpf@mit.edu>
2013-07-10 09:45:46 -0400

    bsim: cleaned up the simulation control toolbar.

--- bsim/simulation/controls.js
+++ bsim/simulation/controls.js
@@ -4,15 +4,19 @@ BSim.Controls = function(container, beta) {
     var mGroup = $('<div class="btn-group">');
     var mResetButton = $('<button class="btn btn-reset"><i class="icon-fast-backward"></i></button>');
     var mRunButton = $('<button class="btn btn-run"><i class="icon-play"></i></button>');
+    var mFastRunButton = $('<button class="btn btn-fast-run"><i class="icon-forward"></i></button>');
     var mStepButton = $('<button class="btn btn-step"><i class="icon-step-forward"></i></button>');
-    var mQuantumSize = $('<input type="range" min="1" max="50000" step="100" value="1">');
 
     var toggle_run = function() {
         if(mBeta.isRunning()) {
             mBeta.stop();
         } else {
-            mBeta.run(mQuantumSize.val());
+            mBeta.run(1);
         }
+    };
+
+    var handle_fast_run = function() {
+        mBeta.run(25000); // Subject to tweaking. Very large values may cause UI sluggishness on slow browsers.
     };
 
     var handle_step = function() {
@@ -26,21 +30,22 @@ BSim.Controls = function(container, beta) {
     var beta_run_start = function() {
         mRunButton.find('i').removeClass('icon-play').addClass('icon-pause');
         mStepButton.attr("disabled", "disabled");
-        mQuantumSize.attr("disabled", "disabled");
+        mFastRunButton.attr("disabled", "disabled");
     };
 
     var beta_run_stop = function() {
         mRunButton.find('i').addClass('icon-play').removeClass('icon-pause');
         mStepButton.removeAttr("disabled");
-        mQuantumSize.removeAttr("disabled");
+        mFastRunButton.removeAttr("disabled");
     };
 
     var initialise = function() {
         mRunButton.click(toggle_run);
         mStepButton.click(handle_step);
         mResetButton.click(handle_reset);
-        mGroup.append(mResetButton, mRunButton, mStepButton);
-        mContainer.addClass('btn-toolbar').append(mGroup, mQuantumSize);
+        mFastRunButton.click(handle_fast_run);
+        mGroup.append(mResetButton, mStepButton, mRunButton, mFastRunButton);
+        mContainer.addClass('btn-toolbar').append(mGroup);
 
         mBeta.on('run:start', beta_run_start);
         mBeta.on('run:stop', beta_run_stop);
