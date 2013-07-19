73e3a5aadd7da5d01cfe60df9169ded31a801216
Antonio Moreno <dontony@mit.edu>
2013-07-19 17:32:47 -0400

    tmsim improvement

--- tmsim/TMSIM.js
+++ tmsim/TMSIM.js
@@ -19,7 +19,6 @@
 			var testRadioButtons = $('<ul>').addClass('test_radio_buttons nav nav-pills');
 			var first = true;
 			for (var tapeName in mTapeList){
-				console.log(tapeName);
 				var tape = mTapeList[tapeName];
 				var radioButton = $('<li>').addClass('test_radio')
 					.append('<a>'+tapeName+'</a>')
@@ -55,8 +54,8 @@
 				})
 				.height(tapeHeight-2)
 			);*/
-			for (var i = 0; i <= numOfSpans+10; i++){
-				tapeDiv.append($('<div>').addClass('tape_span')
+			for (var i = -5; i <= numOfSpans+5; i++){
+				tapeDiv.append($('<div>').addClass('tape_segment')
 					.width(tapeWidth-2)
 					.css({
 						'margin': 0,
@@ -86,7 +85,7 @@
 			
 
 			mContainer.append(radioWrapper, tapeDiv, machineDiv);
-
+			self.move(tapeDiv);
 		}
 		var listToTape = function(tape, tapeDiv){
 			var listToArray = tape.toArray(); 
@@ -94,6 +93,20 @@
 			var array = tape.toArray().array;
 
 
+		}
+		this.move = function(tapeDiv){
+			tapeDiv.find('.tape_segment:first').animate({
+			    left: 0
+			  }, {
+				    duration: 1000,
+				    step: function( now, fx ){
+				    	
+				      tapeDiv.find('.tape_segment:gt(0)' ).each(function(i, e){
+				      	 console.log($(e).css('left'))
+				      		$(e).css( "left", parseInt($(e).css('left'))+now );
+				      });
+			    	}
+			  });
 		}
 		self.initialise();
 	};
