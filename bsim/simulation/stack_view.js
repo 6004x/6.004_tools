edb42e0df58c62cb68f90ba245d0f9f6cbe842ef
Matthew Powers-Freeling <mattpf@mit.edu>
2013-08-12 12:58:26 -0400

    Added access highlighting to the stack view.
    TODO: refactor and deduplicate this code.

--- bsim/simulation/stack_view.js
+++ bsim/simulation/stack_view.js
@@ -4,6 +4,9 @@ BSim.StackView = function(container, beta) {
     var mTable = null;
     var mLastSP = null;
     var mCurrentAnnotations = [];
+    var mWriteHighlight = [];
+    var mReadHighlight = [];
+    var mCurrentLabelRows = [];
 
     // Handy constants
     var SP = 29;
@@ -102,6 +105,55 @@ BSim.StackView = function(container, beta) {
         mTable.endBatch();
     }
 
+    var beta_write_word = function(address) {
+        handle_beta_thing(address, 'last-write', 'a', mWriteHighlight);
+    };
+
+    var beta_read_word = function(address) {
+        handle_beta_thing(address, 'last-read', 'b', mReadHighlight);
+    };
+
+    var handle_beta_thing = function(address, cls, cls_prefix, list) {
+        mTable.startBatch();
+        var row = address / 4;
+        for (var i = list.length - 1; i >= 0; i--) {
+            mTable.removeRowClass(list[i], cls_prefix + (i));
+            if(i <= 3) mTable.addRowClass(list[i], cls_prefix + (i+1));
+            else mTable.removeRowClass(list[i], cls);
+        };
+        if(list.length > 5) list.pop();
+
+        list.unshift(row);
+        mTable.addRowClass(row, cls);
+        mTable.addRowClass(row, cls_prefix + '0');
+        mTable.endBatch();
+    };
+
+    var beta_bulk_read_word = function(addresses) {
+        mTable.startBatch();
+        _.each(addresses, beta_read_word);
+        mTable.endBatch();
+    };
+
+    var beta_bulk_write_word = function(addresses) {
+        mTable.startBatch();
+        _.each(addresses, beta_write_word);
+        mTable.endBatch();
+    };
+
+    var beta_bulk_change_labels = function(labels) {
+        mTable.startBatch();
+        for (var i = mCurrentLabelRows.length - 1; i >= 0; i--) {
+            mCurrentLabelRows[i].updateCell(mCurrentLabelRows[i], 1, BSim.Common.FormatWord(mCurrentLabelRows[i]*4, 4));
+        };
+        for(var address in labels) {
+            console.log(address);
+            var address = parseInt(address, 10);
+            mTable.updateCell(address / 4, 1, labels[address]);
+        }
+        mTable.endBatch();
+    };
+
     var initialise = function() {
         var height = mContainer.data('height') || 350;
         mTable = new BigTable(container, 170, height, 22, 3);
@@ -111,6 +163,11 @@ BSim.StackView = function(container, beta) {
         mBeta.on('resize:memory', beta_resize_memory);
         mBeta.on('change:register', beta_change_register);
         mBeta.on('change:bulk:register', beta_bulk_change_register);
+        mBeta.on('write:word', beta_write_word);
+        mBeta.on('read:word', beta_read_word);
+        mBeta.on('read:bulk:word', beta_bulk_read_word);
+        mBeta.on('write:bulk:word', beta_bulk_write_word);
+        mBeta.on('change:bulk:labels', beta_bulk_change_labels);
     };
     initialise();
 };
