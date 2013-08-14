8ad0023ad0be5d8d75e90c7b47ed0be3266ca744
Matthew Powers-Freeling <mattpf@mit.edu>
2013-08-14 12:50:32 -0400

    Added simple grunt workflow to minify our included files.

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';{}' // hack that 'works' for both JavaScript and CSS.
+            }
+        },
+        clean: ['built'],
+        copy: {
+            bsim: {src: 'bsim/bsim.html', dest: 'built/bsim.html'},
+            jsim: {src: ['jsim/jsim.html', 'jsim/*.png'], dest: 'built/', flatten: true, expand: true}
+        },
+        uglify: {
+            options: {
+                beautify: {
+                    ascii_only: true,
+                    beautify: false
+                }
+            }
+        },
+        useminPrepare: {
+            bsim: 'bsim/bsim.html',
+            jsim: 'jsim/jsim.html',
+            options: {
+                dest: 'built'
+            }
+        },
+        usemin: {
+            bsim: {
+                src: 'built/bsim.html',
+                options: {type: 'html'}
+            },
+            jsim: {
+                src: 'built/jsim.html',
+                options: {type: 'html'}
+            },
+            options: {
+                dirs: ['built']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', ['copy', 'useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin'])    
+}
